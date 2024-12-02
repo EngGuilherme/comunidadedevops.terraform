66bd69b96ffe91173a1b6a4180bfd7632df4f7d9
Guilherme Moreno Vieira <enggmvieira@gmail.com>
2024-12-02 13:57:31 -0700

    feat: Module managed-node completed with iam role configured

--- modules.tf
+++ modules.tf
@@ -14,3 +14,13 @@ module "eks_cluster_role" {
   public_subnet_1b = module.eks_network.subnet_pub_1b
 
 }
+
+module "managed_node_group" {
+  source         = "./modules/managed-node-group"
+  project_name   = var.project_name
+  tags           = local.tags
+  cluster_name   = module.eks_cluster_role.cluster_name
+  subnet_priv_1a = module.eks_network.subnet_priv_1a
+  subnet_priv_1b = module.eks_network.subnet_priv_1b
+
+}
---
+++ modules/managed-node-group/variables.tf
@@ -0,0 +1,22 @@
+
+variable "project_name" {
+  type        = string
+  description = "Project name to be used in the beggining of all Name tags"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Tags to be added to AWS resources"
+}
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name to create mng"
+}
+variable "subnet_priv_1a" {
+  type        = string
+  description = "Subnet ID from AZ 1a"
+}
+variable "subnet_priv_1b" {
+  type        = string
+  description = "Subnet ID from AZ 1b"
+}
