e5b414a241b27011e4b3f30c5aa82e9be50455f2
Guilherme Moreno Vieira <enggmvieira@gmail.com>
2024-12-02 11:54:06 -0700

    Feat: Modification of role_arn in cluster.tf e inclusion of output and sg-rule at cluster module

--- modules/cluster/cluster.tf
+++ modules/cluster/cluster.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "${var.project_name}-eks-cluster"
-  role_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids = [
